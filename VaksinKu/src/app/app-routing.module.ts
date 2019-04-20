b28396484d377b1eb2d6a4b080a797aaa8ccb3d1
kiplikipli <zulkifli_000@apps.ipb.ac.id>
2019-04-20 22:09:13 +0700

    Tambah beberapa page
    Itu tabs page nya belum bisa yee nanti gua benerin

--- VaksinKu/src/app/app-routing.module.ts
+++ VaksinKu/src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuardService } from './services/auth-guard.service'
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: './public/login/login.module#LoginPageModule' },
   { path: 'register', loadChildren: './public/register/register.module#RegisterPageModule' },
   { path: 'posyandu', loadChildren: './members/posyandu/posyandu.module#PosyanduPageModule' },
@@ -12,7 +11,13 @@ const routes: Routes = [
     path : 'members',
     canActivate : [AuthGuardService],
     loadChildren: './members/member-routing.module#MemberRoutingModule'
-  }
+  },
+  { path: 'forgot-password', loadChildren: './public/forgot-password/forgot-password.module#ForgotPasswordPageModule' },
+  { path: 'home', loadChildren: './tabs/home/home.module#HomePageModule' },
+  { path: 'notifications', loadChildren: './tabs/notifications/notifications.module#NotificationsPageModule' },
+  { path: 'profile', loadChildren: './tabs/profile/profile.module#ProfilePageModule' },
+  { path: 'slides', loadChildren: './slides/slides.module#SlidesPageModule' },
+  { path: 'tabs', loadChildren: './tabs/tabs/tabs.module#TabsPageModule' }
 ];
 
 @NgModule({
